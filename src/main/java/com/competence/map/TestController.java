13b1df3439c07e268a6dc83979a51a511c5e5a4e
Nikita Parashchich <nparashchich@lenovo.com>
2021-03-04 13:11:36 +0200

    Test controller for vps hosting

---
+++ src/main/java/com/competence/map/TestController.java
@@ -0,0 +1,16 @@
+package com.competence.map;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test-vps")
+public class TestController {
+
+  @GetMapping
+  public List<String> test() {
+    return List.of("Hello", "World", "!");
+  }
+}
